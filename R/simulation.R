1926a190ab0ea3ff17f0b0893955d45a70088f28
IRZ Pascal <pascal.irz@ofb.gouv.fr>
2025-05-23 16:30:15 +0200

    Ajout fonction tester_tendance() qui permet de multiplier les simulations pour construire un histogramme des pentes

--- R/simulation.R
+++ R/simulation.R
@@ -1,19 +1,19 @@
 #################################
 #' Title
 #'
-#' @param df 
+#' @param modele 
 #'
 #' @return
 #' @export
 #'
 #' @examples
-get_model_coef <- function(df) {
-  mod <- glmer(statut_presence ~ annee_perm +
-                 (1|code_site),
-               family=binomial(link = "logit"), data = df)
+get_model_coef <- function(modele) {
+  # mod <- glmer(statut_presence ~ annee_perm +
+  #                (1|code_site),
+  #              family=binomial(link = "logit"), data = df)
   
-  coef_annee <- coef(summary(mod))[2,1]
-  pvalue_annee <- coef(summary(mod))[2,4]
+  coef_annee <- coef(summary(modele))[2,1]
+  pvalue_annee <- coef(summary(modele))[2,4]
   
   resultat <- data.frame(coef_annee, pvalue_annee)
   
@@ -38,12 +38,12 @@ ajouter_presences(vec, n=0)
 ajouter_presences(vec, n=1)
 ajouter_presences(vec, n=2)
 
-df2 <- df %>% 
-  ungroup() %>% 
-  mutate(statut_presence2 = ajouter_presences(statut_presence, n = 3))
-
-table(df$statut_presence)
-table(df2$statut_presence2)
+# df2 <- df %>% 
+#   ungroup() %>% 
+#   mutate(statut_presence2 = ajouter_presences(statut_presence, n = 3))
+# 
+# table(df$statut_presence)
+# table(df2$statut_presence2)
 
 
 ##########################################
@@ -58,3 +58,40 @@ ajouter_presences_annuelles <-
     df2
     
   }
+
+
+############################################
+tester_tendance <- function(df, n_pres_suppl_par_an = 0, n_permutations = 10) {
+  
+  # test pour un modèle après permutation et injection de la tendance
+  test_1_modele <- function(df, n_pres_suppl_par_an, index = 1) {
+    
+    df_permute <- df %>% 
+      group_by(code_site) %>% 
+      mutate(annee_perm = gtools::permute(annee)) %>% 
+      ungroup() %>% 
+      mutate(annee_index = annee_perm - min(annee_perm))
+    
+    # AJOUT DU SIGNAL CONNU
+    df_trend <- df_permute %>% 
+      ajouter_presences_annuelles(n_pres_suppl_par_an = n_pres_suppl_par_an)
+    
+    mod <- glmer(statut_presence_sim ~
+                   annee_perm +
+                   (1|code_site),
+                 family=binomial(link = "logit"),
+                 data = df_trend)
+    
+    coef_annee <- coef(summary(mod))[2,1]
+    pvalue_annee <- coef(summary(mod))[2,4]
+    
+    resultat <- data.frame(index, n_pres_suppl_par_an, coef_annee, pvalue_annee)
+  }
+  
+  map_df(.x = 1:n_permutations,
+         .f = test_1_modele,
+         df = df,
+         n_pres_suppl_par_an = n_pres_suppl_par_an)
+  
+}
+
--- code/simulations.R
+++ code/simulations.R
@@ -1,114 +1,100 @@
-I(X1^2)
-mod3.3 <- glmer(statut_presence~
-                  scale(annee)+
-              #    scale(I(annee^2))+
-                  (1|code_site),
-                family=binomial(link = "cauchit"), data = base_j25_year_numeric)
+# nettoyage de l'espace de travail et chargement des packages
+rm(list=ls())
+library(tidyverse)
 
+# chargement des données et des fonctions
+load(file = "processed_data/base_j25_year_numeric.rda")
+source(file = "R/simulation.R")
 
-
-df <- cbind(base_j25_year_numeric,
-            predict(mod3.3))
-
-y=predict(mod3.3,se=TRUE)
-
-plot(predict(mod3.3),residuals(mod3.3),col=c("blue","red")[1+as.numeric(base_j25_year_numeric$statut_presence)])
-
-abline(h=0,lty=2,col="grey")
-
-lines(lowess(predict(mod3.3),residuals(mod3.3)),col="black",lwd=2)
-segments(predict(mod3.3),y$fit+2*y$se.fit,predict(mod3.3),y$fit-2*y$se.fit,col="green")
-
-
-plot(scale(base_j25_year_numeric$annee),residuals(mod3.3),col=c("blue","red"))
-lines(lowess(X2,residuals(reg)),col="black",lwd=2)
-> lines(lowess(X2[Y==0],residuals(reg)[Y==0]),col="blue")
-> lines(lowess(X2[Y==1],residuals(reg)[Y==1]),col="red")
-> abline(h=0,lty=2,col="grey")
-####################
-mod3.0 <- glmer(statut_presence~annee+(1|code_site), family=binomial(link = "logit"), data = base_j25_year_numeric)
-
+# préparation du df
 df <- base_j25_year_numeric %>% 
   mutate(code_site = as.factor(code_site),
          statut_presence = as.integer(statut_presence) - 1,
          date_annee = lubridate::yday(date_visite))
 
-mod3.0c <- mgcv::gam(statut_presence ~ annee +
-                       s(date_annee, bs = "ad") +
-                    #  date_annee + 
-                       s(code_site, bs = 're'), 
-                    data = df,
-                    method = 'REML')
-
-summary(mod3.0c)
-plot(mod3.0c)
-
-df2 <- df %>% 
-  cbind(predict(mod3.0c))
-
-sites_tjs_abs <- df2 %>% 
-  group_by(code_site) %>% 
-  summarise(nb_obs = n(),
-            nb_pres = sum(statut_presence)) %>% 
-  ungroup() %>% 
-  mutate(taux_pres = nb_pres / nb_obs) %>% 
-  filter(taux_pres < 0.1) %>% 
-  pull(code_site)
-  
-df3 <- df %>% 
-  filter(!(code_site %in% sites_tjs_abs))
-
-mod3.0c <- mgcv::gam(statut_presence ~ annee +
-                       # s(date_annee, bs = "ad") +
-                         date_annee + 
-                       s(code_site, bs = 're'), 
-                     data = df3,
-                     method = 'REML')
-
-summary(mod3.0c)
-plot(mod3.0c)
-
-#########################################################
-
+#####################################
+# EFFACEMENT DU SIGNAL INTERANNUEL
+# on permute les années pour effacer tout signal de tendance interannuelle
+# l'année permutée est la variable annee_perm
 df_rand <- df %>% 
   group_by(code_site) %>% 
-  mutate(annee_perm = gtools::permute(annee))
+  mutate(annee_perm = gtools::permute(annee)) %>% 
+  ungroup() %>% 
+  mutate(annee_index = annee_perm - min(annee_perm))
 
-
-mod4.0 <- glmer(statut_presence~annee_perm+(1|code_site), family=binomial(link = "logit"), data = df_rand)
-coef_annee <- coef(summary(mod4.0))[2,1]
-pvalue_annee <- coef(summary(mod4.0))[2,4]
+# vérification. On ne doit pas avoir de pente année significative (sauf une fois sur 20)
+mod <- glmer(statut_presence~annee_perm+(1|code_site), family=binomial(link = "logit"), data = df_rand)
+summary(mod)
 
 #################################
-get_model_coef <- function(df) {
-  mod <- glmer(statut_presence ~ annee_perm +
-                 (1|code_site),
-               family=binomial(link = "logit"), data = df)
-  
-   coef_annee <- coef(summary(mod))[2,1]
-   pvalue_annee <- coef(summary(mod))[2,4]
-   
-   resultat <- data.frame(coef_annee, pvalue_annee)
-  
-  
-}
+# AJOUT DU SIGNAL CONNU
+df_trend1 <- df_rand %>% 
+  ajouter_presences_annuelles(n_pres_suppl_par_an = 0.1)
+
+# vérifications
+table(df_trend1$annee)
+table(df_trend1$annee_perm[df_trend1$statut_presence == 1])
+table(df_trend1$annee_perm[df_trend1$statut_presence_sim == 1])
+
+#################################
+# TEST : le modèle permet-il de détercter la tendance ?
+mod_tend1 <- glmer(statut_presence_sim ~
+                     annee_perm +
+                     (1|code_site),
+                   family=binomial(link = "logit"),
+                   data = df_trend1)
+summary(mod_tend1)
+
+# on essaye avec un peu plus de tendance
+df_trend1 <- df_rand %>% 
+  ajouter_presences_annuelles(n_pres_suppl_par_an = 0.2)
+
+mod_trend1 <- glmer(statut_presence_sim ~
+                     annee_perm +
+                     (1|code_site),
+                   family=binomial(link = "logit"),
+                   data = df_trend1)
+summary(mod_trend1)
+
+table(df_trend1$annee_perm[df_trend1$statut_presence == 1])
+table(df_trend1$annee_perm[df_trend1$statut_presence_sim == 1])
+
+# on essaye avec un peu plus de tendance
+df_trend1 <- df_rand %>% 
+  ajouter_presences_annuelles(n_pres_suppl_par_an = 0.4)
+
+mod_trend1 <- glmer(statut_presence_sim ~
+                      annee_perm +
+                      (1|code_site),
+                    family=binomial(link = "logit"),
+                    data = df_trend1)
+summary(mod_trend1)
+test <- get_model_coef(modele = mod_trend1)
+
+table(df_trend1$annee_perm[df_trend1$statut_presence == 1])
+table(df_trend1$annee_perm[df_trend1$statut_presence_sim == 1])
+
+################################################
+# 
 
 
-mod2 <- get_model_coef(df = df_rand)
-#####################################
-ajouter_presence <- function(df, n_pres_par_an = 1) {
-  
-  df2 <- df %>% 
-    filter(statut_presence == 0) %>% 
-    group_by(annee) %>% 
-      mutate(statut_presence2 = )
-    
-    
-    
-  absences_index <- df$statut_presence != 1
-  absences_index[FALSE]
-  df2 <- df %>% 
-    
-    
-  
-}
+# 
+# 
+# 
+# test2 <- test_1_modele(df = df,
+#                        n_pres_suppl_par_an = 0.2)
+# test2
+
+
+test2 <- tester_tendance(df = df,
+                         n_pres_suppl_par_an = 0.,
+                         n_permutations = 100)
+
+test2 %>% 
+  ggplot(aes(x = coef_annee,
+             fill = (pvalue_annee < 0.05))) +
+  geom_histogram(alpha = 0.5, bins = 15) +
+  facet_wrap(~(pvalue_annee < 0.05)) +
+  geom_vline(xintercept = 0, col = "red")
+
+             
